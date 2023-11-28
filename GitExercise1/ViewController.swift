13ff2d62bdf5ccbc6021a1d03eb5cfbe3b997c19
Maks Marozau <licwin11@yandex.ru>
2023-11-28 15:25:59 +0300

    Add webView and Marks

--- GitExercise1/ViewController.swift
+++ GitExercise1/ViewController.swift
@@ -1,17 +1,38 @@
-//
-//  ViewController.swift
-//  GitExercise1
-//
-//  Created by Maks on 28.11.23.
-//
-
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+//MARK: - Class ViewController
 
+final class ViewController: UIViewController {
+
+    
+//MARK: - Properties
+    
+    private let webView = WKWebView()
+
+    
+    
+//MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemGray5
+        
+        view.addSubview(webView)
+        
+        constraintes()
+    }
+    
+    
+//MARK: - Constraintes
+    
+    private func constraintes() {
+        
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30).isActive = true
+        webView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15).isActive = true
+        webView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15).isActive = true
+        webView.heightAnchor.constraint(equalToConstant: 300).isActive = true
     }
 
 
